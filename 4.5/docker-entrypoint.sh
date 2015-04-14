28ea20cfaa502a025f1544a742ac3e358fab1ba5
k-sugisawa <Android!23>
2015-04-14 16:46:34 +0900

    initial commit

---
+++ 4.5/docker-entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+
+if [ -d /docker-entrypoint-init.d ]; then
+  for f in /docker-entrypoint-init.d/*.sh; do
+    [ -f "$f" ] && . "$f"
+  done
+fi
+
+function start() {
+  echo 'Starting SonarQube...'
+  set +e
+  ${SONARQUBE_HOME}/bin/linux-x86-64/sonar.sh start
+  tail -f ${SONARQUBE_HOME}/logs/sonar.log
+}
+
+function help() {
+  echo "Available options:"
+  echo " start          - Starts the SonarQube server (default)"
+  echo " help           - Displays the help"
+  echo " [command]      - Execute the specified linux command eg. bash."
+}
+
+case "$1" in
+  start)
+    start
+    ;;
+  help)
+    help
+    ;;
+  *)
+    if [ -x $1 ]; then
+      $1
+    else
+      prog=$(which $1)
+      if [ -n "${prog}" ] ; then
+        shift 1
+        $prog $@
+      else
+        help
+      fi
+    fi
+    ;;
+esac
+
+exit 0
