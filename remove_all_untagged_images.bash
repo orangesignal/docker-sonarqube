f4b22b12412b06af0a5f24bc406d6d9b1927d7c0
orangesignal <sugisawa@kca.biglobe.ne.jp>
2015-04-15 13:46:21 +0900

    update

--- remove_all_untagged_images.bash
+++ remove_all_untagged_images.bash
@@ -1,2 +1,3 @@
 #!/bin/bash -e
-docker rmi -f $(docker images | grep "^<none>" | awk "{print $3}")
+#docker rmi -f $(docker images | grep "^<none>" | awk "{print $3}")
+docker rmi $(docker images --filter "dangling=true" -q)
