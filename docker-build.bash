4824de860a34e68b3b3c5f1c71b86e8303593f25
orangesignal <sugisawa@kca.biglobe.ne.jp>
2015-04-15 18:24:29 +0900

    update

--- docker-build.bash
+++ docker-build.bash
@@ -12,21 +12,18 @@ function buildAndPushIfNeed() {
   echo "building $$user/$repo:$build_ver"
 
   # build
-  local before_image_id=$(echo $images | grep $build_ver | awk '{print $3}')
   docker build -t $user/$repo:$build_ver $build_dir
-  local after_image_id=$(echo $images | grep $build_ver | awk '{print $3}')
 
-  if test $before_image_id != $after_image_id; then
-    # push
-    echo "$user/$repo:$build_ver $before_image_id -> $after_image_id"
-    docker push $user/$repo:$build_ver
+  # push
+  echo "$user/$repo:$build_ver $before_image_id -> $after_image_id"
+  docker push $user/$repo:$build_ver
 
-    if test "$latest" == 'latest'; then
-      docker tag $user/$repo:$build_ver $user/$repo:latest
-      docker push $user/$repo:latest
-    fi
-  else
-    echo "$user/$repo:$build_ver no change"
+  if test "$latest" == 'latest'; then
+    set +e
+    docker rmi -f $user/$repo:latest
+    set -e
+    docker tag $user/$repo:$build_ver $user/$repo:latest
+    docker push $user/$repo:latest
   fi
 }
 
