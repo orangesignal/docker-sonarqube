d0ded74c9080e68f95ee7e02da921035cc55ef99
orangesignal <sugisawa@kca.biglobe.ne.jp>
2015-04-18 14:52:22 +0900

    update

--- example/docker-entrypoint.sh
+++ example/docker-entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
-# Configure wrapper.conf
-echo 'wrapper.java.additional.2=-Djava.net.preferIPv4Stack=true' >> ${SONARQUBE_HOME}/conf/wrapper.conf
-
 # Tuning the Web Server
 sed -i 's|^#sonar.web.javaOpts=|sonar.web.javaOpts=-server |g' ${SONARQUBE_HOME}/conf/sonar.properties
 
