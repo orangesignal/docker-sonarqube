4089b315d9bb582e85bea86f5c5df243e7f44fb3
orangesignal <sugisawa@kca.biglobe.ne.jp>
2015-04-18 23:16:44 +0900

    update

--- example/docker-entrypoint.sh
+++ example/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 # Tuning the Web Server
 sed -i 's|^#sonar.web.javaOpts=|sonar.web.javaOpts=-server |g' ${SONARQUBE_HOME}/conf/sonar.properties
+sed -i 's|^#sonar.web.context=|sonar.web.context=/sonarqube|g' ${SONARQUBE_HOME}/conf/sonar.properties
 
 # Disbale update center
 sed -i 's|^#sonar.updatecenter.activate=true|sonar.updatecenter.activate=false|g' ${SONARQUBE_HOME}/conf/sonar.properties
